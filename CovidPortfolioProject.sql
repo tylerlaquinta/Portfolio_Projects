77bf6fb109b8f9c77d7184ad36a53930c060b53a
tylerlaquinta <75007776+tylerlaquinta@users.noreply.github.com>
2021-08-16 10:38:44 -0400

    Update CovidPortfolioProject.sql

--- CovidPortfolioProject.sql
+++ CovidPortfolioProject.sql
@@ -1,64 +1,68 @@
---SELECT * FROM
---PortfolioProject..CovidDeaths
---ORDER BY 3,4;
+use PortfolioProject;
 
 SELECT location, date, total_cases, new_cases, total_deaths, population
 FROM CovidDeaths
 ORDER BY 1, 2;
 
--- Looking at Total Cases Vs Total Deaths
 
-SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases) *100 AS DeathPercentage
+-- Looking at Total Cases Vs Total Deaths in United States
+
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases) *100 AS death_percentage
 FROM CovidDeaths
 WHERE location LIKE '%States%'
 ORDER BY 1, 2;
 
 
 -- Total Cases Vs Population
--- What percent of population got covid
-SELECT location, date, population, total_cases, ROUND(((total_cases/population)*100), 2) AS InfectionRate
+-- What percent of population got covid in United States
+
+SELECT location, date, population, total_cases, ROUND(((total_cases/population)*100), 2) AS infection_rate
 FROM CovidDeaths
 WHERE location LIKE '%States%'
 ORDER BY 1, 2;
 
-use PortfolioProject;
 
 -- Countries with highest infection rate
-SELECT location, date, population, total_cases, ROUND(((total_cases/population)*100), 2) AS InfectionRate
+
+SELECT location, date, population, total_cases, ROUND(((total_cases/population)*100), 2) AS infection_rate
 FROM CovidDeaths
 WHERE date = (SELECT MAX(date) FROM CovidDeaths) AND continent IS NOT NULL
-ORDER BY InfectionRate DESC;
+ORDER BY infection_rate DESC;
 
-SELECT location, population, MAX(total_cases), (MAX((total_cases/population))*100) AS InfectionRate
+
+SELECT location, population, MAX(total_cases), (MAX((total_cases/population))*100) AS infection_rate
 FROM CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY location, population
-ORDER BY InfectionRate DESC;
+ORDER BY infection_rate DESC;
 
 
 -- Death Count
-SELECT location, population, MAX(CAST(total_deaths AS INT)) AS TotalDeathCount
+
+SELECT location, population, MAX(CAST(total_deaths AS INT)) AS total_death_count
 FROM CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY location, population
-ORDER BY TotalDeathCount DESC;
+ORDER BY total_death_count DESC;
+
 
 -- Death Percentage
-SELECT location, population, MAX(CAST(total_deaths AS INT)) AS TotalDeathCount,
-(MAX((CAST(total_deaths AS INT)))/population)*100 AS DeathPercentage
+
+SELECT location, population, MAX(CAST(total_deaths AS INT)) AS total_death_count,
+       (MAX((CAST(total_deaths AS INT)))/population)*100 AS death_percentage
 FROM CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY location, population
-ORDER BY DeathPercentage DESC;
+ORDER BY death_percentage DESC;
 
 
 -- Death Count by Continent
 
-SELECT location, MAX(CAST(total_deaths AS INT)) AS TotalDeathCount
+SELECT location, MAX(CAST(total_deaths AS INT)) AS total_death_count
 FROM CovidDeaths
 WHERE continent IS NULL
 GROUP BY location
-ORDER BY TotalDeathCount DESC;
+ORDER BY total_death_count DESC;
 
 
 
@@ -77,11 +81,11 @@ ORDER BY 1, 2;
 
 
 
--- Total Population Vs Population
+-- Total Population Vs Vaccination Population
 
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
-SUM(CAST(vac.new_vaccinations AS INT)) OVER 
-(PARTITION BY dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+       SUM(CAST(vac.new_vaccinations AS INT)) OVER 
+       (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS rolling_people_vaccinated
 FROM CovidDeaths dea
 JOIN CovidVaccinations vac
 	ON dea.location = vac.location
@@ -91,19 +95,19 @@ ORDER BY 2, 3;
 
 -- USE CTE
 
-With PopvsVac (continent, location, date, population, new_vaccinations, RollingPeopleVaccinated)
+With PopvsVac (continent, location, date, population, new_vaccinations, rolling_people_vaccinated)
 AS 
 (
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
-SUM(CAST(vac.new_vaccinations AS INT)) OVER 
-(PARTITION BY dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+       SUM(CAST(vac.new_vaccinations AS INT)) OVER 
+       (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS rolling_people_vaccinated
 FROM CovidDeaths dea
 JOIN CovidVaccinations vac
 	ON dea.location = vac.location
 	AND dea.date = vac.date
 WHERE dea.continent IS NOT NULL
 )
-SELECT *, (RollingPeopleVaccinated/population)*100
+SELECT *, (rolling_people_vaccinated/population)*100
 FROM PopVsVac;
 
 
@@ -112,25 +116,25 @@ FROM PopVsVac;
 DROP TABLE IF EXISTS #PercentPopulationVaccinated
 CREATE TABLE #PercentPopulationVaccinated
 (
-Continent NVARCHAR(255),
-Location NVARCHAR(255),
-Date DATETIME,
-Population NUMERIC,
-New_Vaccinations NUMERIC,
-RollingPeopleVaccinated NUMERIC
+continent NVARCHAR(255),
+location NVARCHAR(255),
+date DATETIME,
+population NUMERIC,
+new_Vaccinations NUMERIC,
+rolling_people_vaccinated NUMERIC
 )
 
 INSERT INTO #PercentPopulationVaccinated
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
 SUM(CAST(vac.new_vaccinations AS INT)) OVER 
-(PARTITION BY dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+(PARTITION BY dea.location ORDER BY dea.location, dea.date) AS rolling_people_vaccinated
 FROM CovidDeaths dea
 JOIN CovidVaccinations vac
 	ON dea.location = vac.location
 	AND dea.date = vac.date
 --WHERE dea.continent IS NOT NULL
 
-SELECT *, (RollingPeopleVaccinated/Population)*100
+SELECT *, (rolling_people_vaccinated/population)*100
 FROM #PercentPopulationVaccinated;
 
 
@@ -140,7 +144,7 @@ FROM #PercentPopulationVaccinated;
 CREATE VIEW PercentPopulationVaccinated AS
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
 SUM(CAST(vac.new_vaccinations AS INT)) OVER 
-(PARTITION BY dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+(PARTITION BY dea.location ORDER BY dea.location, dea.date) AS rolling_people_vaccinated
 FROM CovidDeaths dea
 JOIN CovidVaccinations vac
 	ON dea.location = vac.location
@@ -149,4 +153,4 @@ WHERE dea.continent IS NOT NULL
 --ORDER BY 2,3
 
 SELECT * FROM
-PercentPopulationVaccinated
+PercentPopulationVaccinated;
