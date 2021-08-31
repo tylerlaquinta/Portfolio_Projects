11bd73f754732690826cf57387ff7bda708cec37
tylerlaquinta <75007776+tylerlaquinta@users.noreply.github.com>
2021-08-31 15:14:09 -0400

    Update DimCalendar.sql

--- DimCalendar.sql
+++ DimCalendar.sql
@@ -1,24 +1,24 @@
 -- Cleansed DimDateTable
 SELECT
-  [DateKey],
-  [FullDateAlternateKey] AS Date,
+  DateKey,
+  FullDateAlternateKey AS Date,
   --,[DayNumberOfWeek],
-  [EnglishDayNameOfWeek] AS Day,
+  EnglishDayNameOfWeek AS Day,
   --,[SpanishDayNameOfWeek]
   --,[FrenchDayNameOfWeek]
   --,[DayNumberOfMonth]
   --,[DayNumberOfYear],
-  [WeekNumberOfYear] AS WeekNo,
-  [EnglishMonthName] AS Month,
-  LEFT([EnglishMonthName], 3) AS MonthShort,
+  WeekNumberOfYear AS WeekNo,
+  EnglishMonthName AS Month,
+  LEFT(EnglishMonthName], 3) AS MonthShort,
   --,[SpanishMonthName]
   --,[FrenchMonthName],
-  [MonthNumberOfYear] AS MonthNo,
-  [CalendarQuarter] AS Quarter,
-  [CalendarYear] AS Year
---,[CalendarSemester]
---,[FiscalQuarter]
---,[FiscalYear]
---,[FiscalSemester]
+  MonthNumberOfYear AS MonthNo,
+  CalendarQuarter AS Quarter,
+  CalendarYear AS Year
+  --,[CalendarSemester]
+  --,[FiscalQuarter]
+  --,[FiscalYear]
+  --,[FiscalSemester]
 FROM [AdventureWorksDW2019].[dbo].[DimDate]
-WHERE [CalendarYear] >= 2019
+WHERE CalendarYear >= 2019
